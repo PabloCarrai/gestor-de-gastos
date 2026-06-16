a8daf769602d0b6925a085bad9a882cd219163e1
PabloCarrai <pablohernanjuan@hotmail.com>
2026-06-16 20:32:35 -0300

    va boton de ventana de visualizar gasto

--- views/ventanaprincipal.py
+++ views/ventanaprincipal.py
@@ -10,7 +10,7 @@ class VentanaPrincipal:
         self.ventana = tk.Tk()
 
         self.ventana.title("Ventana Principal")
-        self.ventana.geometry("200x150")
+        self.ventana.geometry("200x200")
 
         self.lbl_principal = tk.LabelFrame(self.ventana, text="Accesos")
         self.lbl_principal.grid(column=0, row=0, padx=10, pady=10)
@@ -20,11 +20,19 @@ class VentanaPrincipal:
         )
         self.btn_ingresar_gasto.grid(column=0, row=0, padx=10, pady=10)
 
+        self.btn_ver_gasto = tk.Button(
+            self.lbl_principal, text="Ver Gasto", command=self.ver_gasto
+        )
+        self.btn_ver_gasto.grid(column=0, row=1, padx=10, pady=10)
+
         self.btn_salir = tk.Button(self.ventana, text="Salir", command=self.salir)
         self.btn_salir.grid(column=0, row=1, padx=10, pady=10)
 
     def ingresar_gasto(self):
         self.ventana_ingreso = VentanaIngresoDatos()
+
+    def ver_gasto(self):
+        pass
 
     def salir(self):
         self.ventana.destroy()
