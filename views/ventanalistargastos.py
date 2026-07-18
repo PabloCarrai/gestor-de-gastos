90e3d9594b73ebc959f178d3baf8769a25be8ca5
PabloCarrai <pablohernanjuan@hotmail.com>
2026-07-18 01:38:16 -0300

    La parte del objeto que labura las consultas e inserciones en la db para la tabla principal estan mal armadas. Tendria que rehacerlo desde cero para que quede bien. Onda el init unicamente pida direccion de la db, y los metodos pidan lo que necesiten

--- views/ventanalistargastos.py
+++ views/ventanalistargastos.py
@@ -2,11 +2,12 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox as ms
+from models.funciones_db import GestorDB
 
 
 class VentanaListarDatos:
     """
-    Clase para la venta que lista los gastos
+    Clase para la ventana que lista los gastos
     """
 
     def __init__(self):
@@ -14,15 +15,13 @@ class VentanaListarDatos:
         self.ventana_listado_gasto = tk.Toplevel()
         #   Titulo de la ventana
         self.ventana_listado_gasto.title("Listado de Gastos")
-        self.ventana_listado_gasto.geometry("400x500")
+
         self.lbl_detalle_listado_gasto = tk.LabelFrame(
             self.ventana_listado_gasto, text="Detalle"
         )
-        self.lbl_detalle_listado_gasto.grid(column=0, row=0, padx=10, pady=10)
-        self.txt_prueba = tk.Label(self.lbl_detalle_listado_gasto, text="Probando")
-        self.txt_prueba.grid(column=0, row=1, padx=10, pady=10)
+        self.lbl_detalle_listado_gasto.grid(column=0, row=1, padx=10, pady=10)
 
-        columnas = ("Nombre", "Edad", "Cargo")
+        columnas = ("Fecha", "Descripcion", "Monto", "Categoria")
         self.tv_listado_datos = ttk.Treeview(
             self.lbl_detalle_listado_gasto, columns=columnas, show="headings"
         )
@@ -30,3 +29,21 @@ class VentanaListarDatos:
         for columna in columnas:
             self.tv_listado_datos.heading(columna, text=columna)
             self.tv_listado_datos.column(columna, anchor=tk.CENTER)
+
+        self.btn_mostrar_datos = tk.Button(
+            self.ventana_listado_gasto,
+            text="Mostrar Info",
+            command=self.cargar_registros,
+        )
+        self.btn_mostrar_datos.grid(column=0, row=5, padx=10, pady=10)
+
+    def cargar_registros(self):
+        pass
+
+
+"""
+    fecha DATE DEFAULT CURRENT_DATE,
+    descripcion TEXT NOT NULL,
+    monto DECIMAL(10, 2) NOT NULL,
+    categoria TEXT
+"""
